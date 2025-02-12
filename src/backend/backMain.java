22560e5755594a0ab26271f7af72f66f6ea5af3f
jere-dev <jeremthom1@gmail.com>
2025-02-12 00:35:27 -0500

    project structure and hello world

---
+++ src/backend/backMain.java
@@ -0,0 +1,7 @@
+package backend;
+
+public class backMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Backend!");
+    }
+}
---
+++ src/frontend/frontMain.java
@@ -0,0 +1,7 @@
+package frontend;
+
+public class frontMain {
+    public static void main(String[] args) {
+        System.out.println("Hello FrontEnd!");
+    }
+}
