68b94025d3ad3f43f0f24ff7d0e0ee93cd2a2b0d
jere-dev <jeremthom1@gmail.com>
2025-03-14 12:18:56 -0400

    Implement Subject and SubjectManager

---
+++ src/main/java/ca/uoguelph/backend/Subject.java
@@ -0,0 +1,27 @@
+package ca.uoguelph.backend;
+
+public class Subject {
+    private String code;
+    private String name;
+
+    public Subject(String code, String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    protected void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ca/uoguelph/backend/SubjectManager.java
@@ -0,0 +1,46 @@
+package ca.uoguelph.backend;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class SubjectManager {
+    private static ArrayList<Subject> subjects;
+    public static void addSubject(String code, String name) {subjects.add(new Subject(code, name));} //TODO: remove add to excel
+    public static ArrayList<Subject> getSubjects(){return subjects;}
+
+    public static void removeSubject(String code){
+        Subject subject = subjects.stream().filter(s -> s.getCode().equals(code)).findFirst().orElse(null);
+        if(subject == null){throw new IllegalArgumentException("invalid code");}
+        subjects.remove(subject);
+    }
+
+    public static void removeSubject(Subject subject){
+        subjects.remove(subject);
+    }
+
+    public static ArrayList<Subject> searchByName(String name){
+        return subjects.stream().filter(s -> s.getName().contains(name)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static ArrayList<Subject> searchByCode(String code){
+        return subjects.stream().filter(s -> s.getCode().contains(code)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static Subject getSubject(String code){
+        Subject subject = subjects.stream().filter(s -> s.getCode().equals(code)).findFirst().orElse(null);
+        if(subject == null){throw new IllegalArgumentException("invalid code");}
+        return subject;
+    }
+
+    public static void editSubject(String code, String newCode, String newName){
+        Subject subject = subjects.stream().filter(s -> s.getCode().equals(code)).findFirst().orElse(null);
+        if(subject == null){throw new IllegalArgumentException("invalid code");}
+        subject.setCode(newCode);
+        subject.setName(newName);
+    }
+
+    public static void editSubject(Subject subject, String newCode, String newName){
+        subject.setCode(newCode);
+        subject.setName(newName);
+    }
+}
