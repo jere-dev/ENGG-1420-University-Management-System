88b2fe5ffcd55f7250fa95f98574d1868a58db1d
sarah <sarahmsmith126@gmail.com>
2025-03-16 16:52:51 -0400

    border :0

--- src/main/java/test/SideLoader.java
+++ src/main/java/test/SideLoader.java
@@ -5,7 +5,7 @@ import ca.uoguelph.frontend.SideLoaderApp;
 public class SideLoader {
     public static void main(String[] args) {
 
-        SideLoaderApp.fxmlPath = "subject_catalog_user"; // <-- PAGE HERE
+        SideLoaderApp.fxmlPath = "faculty_profile_faculty"; // <-- PAGE HERE
 
         SideLoaderApp.launch(SideLoaderApp.class, args);
     }
