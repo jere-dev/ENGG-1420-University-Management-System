309c4f40d05f780515556bb26474ff69d0b84bbe
180Sai <92890058+180Sai@users.noreply.github.com>
2025-03-18 18:46:25 -0400

    add backend loading and login

--- src/main/java/test/SideLoader.java
+++ src/main/java/test/SideLoader.java
@@ -1,11 +1,17 @@
 package test;
 
+import ca.uoguelph.backend.*;
+import ca.uoguelph.backend.login.LoginManager;
+import ca.uoguelph.backend.login.LoginState;
 import javafx.application.Platform;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 class SideLoader {
     private static final String defaultFileName = "admin/course_manager.fxml"; // <-- PAGE HERE
+    private static final LoginState defaultLoginState = LoginState.ADMIN; // <-- PERMISSION HERE
 
     private static SideLoaderApp loadedApp;
     static SideLoaderApp getLoadedApp() {return loadedApp;}
@@ -14,6 +20,25 @@ class SideLoader {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean isRepeat = true;
+
+        Database.loadExcelSheet(SideLoader.class.getResource("/database/UMS_Data.xlsx").getPath());
+        SubjectManager.loadSubjects();
+        CourseManager.loadCourses();
+        StudentManager.loadStudents();
+        FacultyManager.loadFaculty();
+        EventManager.loadCourses();
+
+        String login = switch (defaultLoginState) {
+            case ADMIN -> "admin";
+            case STUDENT -> "student";
+            case FACULTY -> "faculty";
+        };
+        LoginManager.addUser(new Admin("admin", "admin", "Test@uoguelph.ca", "test", "default"));
+        LoginManager.addUser(new Student("student", "student", "123","123","Test@uoguelph.ca",
+                "test", "default","default",new ArrayList<>(),"",0,"student"));
+        LoginManager.addUser(new Faculty("faculty", "faculty", "default","default",
+                "Test@uoguelph.ca", "test",new ArrayList<>(), "faculty","default"));
+        LoginManager.login(login, login);
 
         Thread windowThread = new Thread(() -> SideLoaderApp.launch(SideLoaderApp.class, args));
         windowThread.setDaemon(true);
