f9c2775823460db9a0e28e26ec6f7f38de9bd256
180Sai <92890058+180Sai@users.noreply.github.com>
2025-03-17 20:10:08 -0400

    add course row preset

--- src/main/java/ca/uoguelph/frontend/objects/table/row/TableRowPreset.java
+++ src/main/java/ca/uoguelph/frontend/objects/table/row/TableRowPreset.java
@@ -1,16 +1,22 @@
 package ca.uoguelph.frontend.objects.table.row;
 
+import ca.uoguelph.backend.Course;
+import ca.uoguelph.backend.Section;
 import ca.uoguelph.backend.Student;
 import ca.uoguelph.backend.Subject;
 import ca.uoguelph.backend.login.LoginManager;
 import ca.uoguelph.backend.login.LoginState;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class TableRowPreset extends AbstractTableRow {
+public class TableRowPreset extends TableRow {
     /**
      * Converts a {@code Subject} object to a table row. Adds an edit button if the {@code LoginState} is an administrator.
      */
@@ -35,19 +41,42 @@ public class TableRowPreset extends AbstractTableRow {
         return List.of(nameLabel, codeLabel, editButton);
     }
 
-//    /**
-//     * Converts a {@code Course} object to a table row. Adds an edit button when the {@code LoginState} is an administrator.
-//     * @param c the course
-//     */
-//    public TableRow(Course c) {
-//        super(courseRow(c));
-//    }
-//
-//    public List<Node> courseRow(Course course) {
-//        // TODO: wait until course exists
-//
-//        return List.of();
-//    }
+    /**
+     * Converts a {@code Course} object to a table row. Specific to when the {@code LoginState} is an administrator.
+     * @param c the course
+     */
+    public TableRowPreset(Course c) {
+        super(courseRow(c));
+    }
+
+    public static List<Node> courseRow(Course c) {
+        if (LoginManager.getRole() != LoginState.ADMIN)
+            throw new IllegalStateException("Attempt to create course list for " + LoginManager.getRole());
+
+        Label titleLabel = new Label(c.getTitle()),
+                codeLabel = new Label(c.getCourseCode());
+        Hyperlink courseLink = new Hyperlink("See profile");
+
+        Label instructorLabel = new Label();
+        instructorLabel.setWrapText(true);
+
+        String[] instructorNames = (String[]) c.getSections().stream()
+                .map(s -> s.getInstructors().stream()).distinct().limit(5).toArray();
+        System.out.println(instructorNames);
+
+//        ArrayList<String> nameList = new ArrayList<>();
+//        HashSet<String> nameSet = new HashSet<>();
+//        c.getSections().stream().forEach(s -> nameSet.addAll(s.getInstructors()));
+
+        StringBuilder nameBuilder = new StringBuilder();
+        for (int i = 0; i < Math.max(instructorNames.length, 4); i++) nameBuilder.append(instructorNames[i]).append(",");
+        nameBuilder.deleteCharAt(nameBuilder.length() - 1);
+        if (instructorNames.length > 4) nameBuilder.append("...");
+
+        instructorLabel.setText(nameBuilder.toString());
+
+        return List.of(titleLabel, codeLabel, instructorLabel, courseLink);
+    }
 
     /**
      * Converts a {@code Student} object to a table row. Specific to only when the {@code LoginState} is an administrator.
